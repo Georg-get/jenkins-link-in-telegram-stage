146a1f9b6491518e019abaa83125aee91e6c671c
Georgy <ujif12349@yandex.ru>
2022-05-06 20:40:27 +0000

    Update Telegram.groovy

---
+++ Telegram.groovy
@@ -0,0 +1,8 @@
+stage("Sending a message in Telegram chat") {
+    steps {
+        //withCredentials([usernamePassword(credentialsId: 'telegram_bak', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            sh """
+                curl -s -X POST https://api.telegram.org/bot[${TokenId}]/sendMessage -d chat_id=[${ChatId}] -d text="Новый релиз репы ${GIT_REPO_NAME} , ветки ${BRANCH_NAME}"
+                """
+            }
+        }
