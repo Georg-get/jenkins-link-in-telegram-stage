546432eba9a7990e28abd3af74c8279a02a9e6b1
Георгий Червяков <georgijcervakov@MacBook-Air-Georgij.local>
2022-05-12 18:12:48 +0300

    Добавил правки в пайп

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,49 +5,50 @@ pipeline {
         buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10', daysToKeepStr: '2', artifactDaysToKeepStr: '2'))
         timestamps()
     }
-
-    stage("build") {
-        stage {
-            sh  """
-                """
-        }
-    }    
-
-    stage("test") {
-        stage {
-            sh  """
-                """
-        }
-    }
-
-    stage("sonar") {
-        stage {
-            sh  """
-                """
-        }
-    }
-
-    stage("build docker image") {
-        stage {
-            sh  """
-                """
-        }
-    }
-
-    stage("deploy") {
-        stage {
-            sh  """
-                """
-        }
-    }
-
-    stage("Sending a message in Telegram chat") {
-        steps {
-            withCredentials([usernamePassword(credentialsId: 'telegram_bak', usernameVariable: 'ChatId', passwordVariable: 'TokenId')]) {
-                sh """
-                    curl -s -X POST https://api.telegram.org/bot[${TokenId}]/sendMessage -d chat_id=[${ChatId}] -d text="Новый релиз репы ${GIT_REPO_NAME} , ветки ${BRANCH_NAME}"
+    stages {
+        stage("build") {
+            stage {
+                sh  """
                     """
-                }
-            }   
+            }
+        }    
+
+        stage("test") {
+            stage {
+                sh  """
+                    """
+            }
+        }
+
+        stage("sonar") {
+            stage {
+                sh  """
+                    """
+            }
+        }
+
+        stage("build docker image") {
+            stage {
+                sh  """
+                    """
+            }
+        }
+
+        stage("deploy") {
+            stage {
+                sh  """
+                    """
+            }
+        }
+
+        stage("Sending a message in Telegram chat") {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'telegram_bak', usernameVariable: 'ChatId', passwordVariable: 'TokenId')]) {
+                    sh """
+                        curl -s -X POST https://api.telegram.org/bot[${TokenId}]/sendMessage -d chat_id=[${ChatId}] -d text="Новый релиз репы ${GIT_REPO_NAME} , ветки ${BRANCH_NAME}"
+                        """
+                    }
+                }   
+        }
     }
 }
